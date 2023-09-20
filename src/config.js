9bb3084a616b3aed1167bcf93df02d2c32651f71
Zpriter <zpriter@hotmail.com>
2023-09-19 19:04:47 -0500

    hola

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
 config();
 
-export const PORT = process.env.PORT || 4000
+export const PORT = process.env.PORT || 5000
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app.js';
 import { PORT } from './config.js'
-
+console.log("Hola_2")
 app.listen(PORT);
 console.log("servidor corriendo en puerto", PORT);
